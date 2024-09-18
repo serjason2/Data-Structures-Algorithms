629c4ab28a1a578329ba8c15934d05dceebc07a1
Jason Sourivong <serjason2@gmail.com>
2024-09-17 23:09:31 -0500

    Created SelectionSort.py
    In Progress

---
+++ Python/SelectionSort.py
@@ -0,0 +1,5 @@
+# DSA Selection Sort
+# The selection sort algorithm finds the lowest value in an array and moves it to the front of the array.
+
+# The algorithm looks through the array again and again, moving the next lowest values to the front, until the array is sorted.
+
